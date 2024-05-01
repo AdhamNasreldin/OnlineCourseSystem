8a74653c2f8e430710724993fe8424efce3fea1d
Ahmed Sameh <138621729+aaahhmdd@users.noreply.github.com>
2024-05-01 05:47:08 +0300

    done

---
+++ java project new version/online course system/src/Student.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList ;
+import java.util.Scanner;
+
+public class Student extends User {
+
+    private  int ID =1000 ;						// This function doesn't have a setter as you can't change your ID
+    private  ArrayList <Courses> CoursesEnrolled ; //
+
+
+    public Student() {
+        this.ID += getCounter() ;
+        incrementCounter ();
+    }
+
+    public Student (String name) { // constructor sare3
+        super(name)  ;
+        this.ID += getCounter() ;
+        incrementCounter ();
+    }
+
+    public Student (String name, String Gender, String mail,int age) { // constructor main
+        super(name,Gender,mail,age);
+        this.ID += getCounter() ;
+        incrementCounter ();
+    }
+
+    public int getID() {
+        return ID;
+    }
+    public void giveRating(int X){
+        System.out.println("choose rating for your instructor from 1 to 10");
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        setRating(X);
+    }
+    public float showGrade() {
+        float w =getGrade();
+        return w;
+    }
+
+    public ArrayList<Courses> getCoursesEnrolled() {
+        return CoursesEnrolled;
+    }
+
+    public void setCoursesEnrolled(ArrayList<Courses> coursesEnrolled) {
+        CoursesEnrolled = coursesEnrolled;
+    }
+
+
+    @Override
+    public void PrintInfo(){
+        System.out.println("Student info : \n");
+        System.out.println("\nname :"+super.getName() +
+                "\nID : " + getID() +
+                "\nGender" + super.getGender() +
+                "\nmail" + super.getMail() +
+                "\nAge" + super.getAge() +
+                "\nGrade" + super.getGrade() +
+                "\nTaken courses" + getCoursesEnrolled()
+        );
+
+    }
+    //import some methods from quizzes
+    Quizzes quizzes = new Quizzes();
+
+    public int getQuizzResult(){
+        return quizzes.getResult();
+    }
+    public ArrayList<String> getQuizzQuestionlist() {
+        quizzes.getQuestionlist();
+        return quizzes.getQuestionlist();
+    }
+    public void setQuizzRecievedAnswer(ArrayList<String> recievedAnswer) {
+        quizzes.setRecievedAnswer(recievedAnswer);
+    }
+
+    //import some methods from practice
+    Practice practice = new Practice();
+
+    public int getPracticeResult(){
+        return practice.getResult();
+    }
+    public ArrayList<String> getPracticeQuestionlist() {
+        practice.getQuestionlist();
+        return practice.getQuestionlist();
+    }
+    public void setPracticeRecievedAnswer(ArrayList<String> recievedAnswer) {
+        practice.setRecievedAnswer(recievedAnswer);
+    }
+}
---
+++ java project new version/online course system/src/User.java
@@ -0,0 +1,79 @@
+public abstract class User {
+
+    private String name ;
+    private String Gender ;
+    private String mail ;
+    private static int Counter = 1 ;  // static int to generate number of objects
+    private int age ;
+    private int rating ;
+    private float grade ;
+
+    public User() { // constructor default
+    }
+
+    public User(String name) { // constructor sryy3
+
+        this.name =name ;
+    }
+
+
+    public User (String name, String Gender, String mail,int age  ) { // constructor main
+        this.name=name ;
+        this.Gender=Gender;
+        this.mail=mail ;
+        this.age=age ;
+    }
+
+
+    public static int getCounter() {
+
+        return Counter ;
+    }
+
+    public static void incrementCounter()
+    {
+        Counter+=1 ;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    public float getGrade() {
+        return grade;
+    }
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+
+    public abstract void PrintInfo();
+
+}
