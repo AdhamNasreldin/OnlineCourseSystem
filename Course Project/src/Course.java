2449f8d60ac8e1387e2a97c03738de05966a0684
Adham Nasreldin <adhamnasreldin16@gmail.com>
2024-05-05 14:24:10 +0300

    arranging project files

--- Course Project/src/Course.java
+++
@@ -1,8 +0,0 @@
-
-public class Course {
- public	String name  ; 
- public 
- Student [] EnrolledStudents ; 
- 
-
-}
--- Course Project/src/Instructor.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class Instructor extends User  {
-	private  ArrayList <Course> CoursesEnrolled ;
-	
-	
-}
